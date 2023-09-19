2fea3b9585a86ae9472a208aed04e1d8483f2847
Shravan Shetty <shettyshravan24@gmail.com>
2023-09-19 15:16:18 +0530

    bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const webpack = require("webpack");
 
-const environment = "development";
+const environment = "production";
 
 module.exports = {
   entry: "./src/index.tsx",
