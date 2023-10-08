ec4732e99b67c3fd893cd0f2db46ae5020ad5592
Shravan Shetty <shettyshravan24@gmail.com>
2023-10-08 19:04:33 +0530

    Login page UI changes and title icon added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import classes from "./../styles/Login.module.css";
+import { Button } from "@swc-react/button";
+import { Icon } from "@swc-react/icon";
+import LoginIcon from "../components/LoginIcon";
 const { v4: uuidv4 } = require("uuid");
 
 function generateRandomString(length: number) {
@@ -25,8 +28,23 @@ function Login(props: any) {
   return (
     <div className={classes.container}>
       <div className={classes["login-card"]}>
-        <h1>SCHOLARSHIP PORTAL</h1>
-        <button onClick={loginWithGoogleSSO}>Login with Google</button>
+        <img
+          className={classes["logo"]}
+          src="../../assets/AbhyudayaLogo.jpeg"
+          alt=""
+        />
+        <h1>Keshava Kripa Samvardhana Samiti</h1>
+        <h2>Abhyudaya Vidyanidhi</h2>
+        <h2>Scholarship Portal</h2>
+        <Button
+          className={classes["login-button"]}
+          onClick={loginWithGoogleSSO}
+        >
+          <Icon slot="icon">
+            <LoginIcon />
+          </Icon>
+          Login with Google
+        </Button>
       </div>
     </div>
   );
